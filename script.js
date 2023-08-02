55bb38ad8cc0dae07500da2d08893cc59ab04d5c
PriyaSandal <133044444+PriyaSandal@users.noreply.github.com>
2023-08-02 14:17:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
-// complete this js code
-function Person(name, age)
-	{
-		this.name=name;
-		this.age=age;
-	}
-		Person.prototype.greet=function()
-		{
-			console.log("Hello, my name is "+ this.name + "and I am "+ this.age + " years old.")
-			
-		}
-		
-	
+// Constructor function for Person
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
 
-function Employee(name, age, jobTitle) 
-{
-	Person.call(this, name, age);
+// Method added to Person prototype
+Person.prototype.greet = function () {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
+
+// Constructor function for Employee
+function Employee(name, age, jobTitle) {
+  // Calling the Person constructor with 'this' context
+  Person.call(this, name, age);
   this.jobTitle = jobTitle;
 }
-Employee.prototype = Object.create(Person.prototype);
 
-// Set the constructor of Employee.prototype back to Employee.
+// Inherit the Person prototype in the Employee prototype
+Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
-// Add the jobGreet() method to the Employee prototype.
+// Method added to Employee prototype
 Employee.prototype.jobGreet = function () {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
 
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
+// Usage:
+const person1 = new Person("John", 30);
+person1.greet(); // Output: Hello, my name is John, I am 30 years old.
+
+const employee1 = new Employee("Alice", 25, "Software Engineer");
+employee1.greet(); // Output: Hello, my name is Alice, I am 25 years old.
+employee1.jobGreet(); // Output: Hello, my name is Alice, I am 25 years old, and my job title is Software Engineer.
